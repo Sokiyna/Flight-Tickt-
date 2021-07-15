5ab7d3c18544fb2579f0917b76774274ed0235ed
bahaamq <bahaa.mq@gmail.com>
2021-07-15 17:21:01 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import toast, { Toaster } from 'react-hot-toast';
+
 import Header from './Components/header';
 import Main from './Components/main';
 function App() {
   return (
     <>
+    <Toaster
+  position="top-center"
+  reverseOrder={false}
+/>
+
+
       <Header />
       <Main />
     </>

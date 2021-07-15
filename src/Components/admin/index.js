a004f65ee87943eada58b7089027f96d779f8488
Sokiyna <sokiynanaser@yahoo.com>
2021-07-15 18:03:39 +0300

    handle db

--- src/Components/admin/index.js
+++ src/Components/admin/index.js
@@ -1,69 +1,278 @@
 import React from 'react';
-import Ticket from './ticket';
-import './admin.css';
+import './home.css';
+import {Navbar} from 'react-bootstrap'
+import {Form,Button,Col} from 'react-bootstrap/'
 import io from 'socket.io-client';
+const SERVER_URL = process.env.SERVER_URL || 'localhost:5000/';
+const socket = io(SERVER_URL, { transports: ['websocket'] });
 
-const socket = io('localhost:5000/', { transports: ['websocket'] });
-class Admin extends React.Component {
+class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      staffName: '',
-      tickets: [],
-      onlineStaff: [],
+      userName: '',
+      msg:"",
+      showMsgs:false,
+      AllMsg:[],
+      msgs:[]
     };
   }
   componentDidMount() {
-    // run once when component is mounted
-    const staffName = prompt("WHAT's your name?");
-    this.setState({ staffName });
+    // const userName = prompt("Can you provide us with your name?");
+    // this.setState({ userName });
+
+    //clientName
     socket.on('connect', () => {
-      //1a
-      socket.emit('join', { name: staffName });
-      socket.on('newTicket', (payload) => {
-        this.setState({ tickets: [...this.state.tickets, payload] }); 
-      });
-      socket.on('onlineStaff', (payload) => {
-        this.setState({ onlineStaff: [...this.state.onlineStaff, payload] });
-      });
-      socket.on('offlineStaff', (payload) => {
-        this.setState({
-          onlineStaff: this.state.onlineStaff.filter((staff) => staff.id !== payload.id),
-        });
+      socket.on('claimed', function (payload) {
+        alert(`${payload.name} ,
+        ${payload.randomNum},
+        ${payload.txt}
+        `
+        );
       });
     });
-  }
-  handleClaim = (id, socketId) => {
-    console.log(socketId);
-    socket.emit('claim', { 
-      id:id,
-       name: this.state.staffName,
-        studentId: socketId,
+
+    socket.on('replaiedtoUsers',  (payload)=> {
+      this.setState({ msgs: [...this.state.msgs, payload] ,
+      showMsgs:true
+      }); 
+  
+  
+    }
+
+
+      )
     
-    });
+  }
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
   };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const payload = {
+      ...this.state,
+      created_at: Date.now().toLocaleString(),
+    };
+    console.log('hello', this.state.Destination);
+
+    socket.emit('sendRequest', payload);  };
+
+  handleMsg=async (e)=>{
+    await this.setState({msg:e.target.value});
+
+  }
+
+
+  updatestate=(e)=>{
+  
+    this.setState({ AllMsg: [...this.state.AllMsg,this.state.msg] })
+    // this.setState({msg:''});
+
+  }
+  sendMsg=async (e)=>{
+       
+    e.preventDefault();
+    await this.updatestate()
+
+
+    const payload = {
+     msg:this.state.msg,
+      created_at: Date.now(),
+      userName:this.state.clientName
+    };
+    console.log(payload.msg)
+    socket.emit('sendmsg', payload);
+
+  }
+
   render() {
     return (
-      <main className="admin-container">
-        <section id="container">
-          <h2>Opened Tickets</h2>
-          <section id="tickets">
-            {this.state.tickets.map((ticket) => {
-              return (
-                <Ticket {...ticket} handleClaim={this.handleClaim} key={ticket.id} />
-              );
-            })}
-          </section>
+      <div >
+      <main className="container">
+        <section className="form-card boxShadow"  >
+     
+
+<Form onSubmit={this.handleSubmit}>
+  <Form.Row>
+    <Form.Group as={Col} controlId="formGridEmail" >
+      <Form.Label>Name</Form.Label>
+      <Form.Control 
+       className="yourName"
+        type="text"
+        name="clientName"
+        placeholder="write your name here..."
+        onChange={this.handleChange}
+      />
+    </Form.Group>
+
+
+
+  <Form.Group controlId="formGridAddress1">
+    <Form.Label>Address</Form.Label>
+    <Form.Control 
+           className="yourName"
+           type="text"
+           name="address"
+           placeholder="write your address here..."
+           onChange={this.handleChange}
+            />
+  </Form.Group>
+
+  {/* <Form.Group controlId="formGridAddress2">
+    <Form.Label>Address 2</Form.Label>
+    <Form.Control placeholder="Apartment, studio, or floor" />
+  </Form.Group> */}
+  </Form.Row>
+
+  <Form.Row>
+    <Form.Group as={Col} controlId="formGridCity">
+      <Form.Label>Phone Number</Form.Label>
+      <Form.Control 
+className="center"        
+   type="text"
+           name="phone"
+           placeholder="write your address here..."
+           onChange={this.handleChange}
+      />
+    </Form.Group>
+
+  </Form.Row>
+  <fieldset >
+    <Form.Group >
+      <Form.Label as="legend">
+      Airlines:
+      </Form.Label>
+      
+        <Form.Check 
+        className="center"
+          type="radio"
+          label="Fly Emirates"
+          name="Airlines"
+          value="Fly Emirates"
+          id="formHorizontalRadios1"
+ onChange={this.handleChange}
+      
+        />
+
+        <Form.Check
+         className="center"
+         type="radio"
+         label="Qatar Airlines"
+         name="Airlines"
+         value="Qatar Airlines"
+         id="formHorizontalRadios1"
+onChange={this.handleChange}
+     
+        />
+        <Form.Check
+     
+          className="center"
+          type="radio"
+          label="Jordanian Royal"
+          name="Airlines"
+          value="Jordanian Royal"
+          id="formHorizontalRadios1"
+ onChange={this.handleChange}
+      
+         
+        />
+   
+    </Form.Group>
+  </fieldset>
+
+
+  <fieldset className="center">
+    <Form.Group>
+      <Form.Label as="legend">
+Destination:       </Form.Label>
+      
+        <Form.Check
+          type="radio"
+          label=" UK"
+          value="UK"
+          name="Destination"
+          id="formHorizontalRadios1"
+ onChange={this.handleChange}
+        />
+
+        <Form.Check
+          type="radio"
+          label="USA"
+          value="USA"
+          name="Destination"
+          id="formHorizontalRadios1"
+ onChange={this.handleChange}
+        />
+        <Form.Check
+          type="radio"
+          label="DE"
+          value="DE"
+          name="Destination"
+          id="formHorizontalRadios1"
+ onChange={this.handleChange}
+        />
+   
+    </Form.Group>
+  </fieldset>
+
+
+
+
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+
+
+
+
+
+
         </section>
-        <aside id="online-staff">
-          <h2>Available TAs</h2>
-          {this.state.onlineStaff.map((staff) => (
-            <h2 key={staff.id}>{staff.name}</h2>
-          ))}
-        </aside>
+
+
       </main>
+
+
+<div className="feedback">
+<h3>Leave A feedback</h3>
+        <input
+            type="text"
+            onChange={this.handleMsg}
+         />
+    <button  onClick={this.sendMsg}>Send </button>
+    
+    
+    <br></br>
+    <h2>test</h2>
+
+{
+  this.state.showMsgs &&
+  
+  this.state.msgs.map((msg)=>{
+    return(
+      <div className="feedbackmsg">
+      <p>{msg.userName2}: {msg.msg2} </p>
+      <p> {"Admin : "} {msg.msg} </p>
+      </div>
+    )
+  })
+}
+
+
+    <Navbar bg="dark" variant="dark">
+    <Navbar.Brand className="centerize">All Rights Reserved</Navbar.Brand>
+  </Navbar>
+
+
+
+  </div> 
+    
+  </div>
+
     );
   }
 }
 
-export default Admin;
+export default Home;
--- src/Components/home/index.js
+++ src/Components/home/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './home.css';
+import { Navbar } from 'react-bootstrap'
+
+import { Form, Button, Col } from 'react-bootstrap/'
 import io from 'socket.io-client';
+import axios from 'axios';
+import { logRoles } from '@testing-library/react';
 const SERVER_URL = process.env.SERVER_URL || 'localhost:5000/';
 const socket = io(SERVER_URL, { transports: ['websocket'] });
 
@@ -9,16 +14,41 @@ class Home extends React.Component {
     super(props);
     this.state = {
       userName: '',
+      msg: "",
+      showMsgs: false,
+      AllMsg: [],
+      msgs: []
     };
   }
   componentDidMount() {
-    const userName = prompt("Can you provide us with your name?");
-    this.setState({ userName });
+    // const userName = prompt("Can you provide us with your name?");
+    // this.setState({ userName });
+
+    //clientName
+
+   
     socket.on('connect', () => {
       socket.on('claimed', function (payload) {
-        alert(`${payload.name} claimed your ticket , hhhhhh${payload.randomNum}`);
+        alert(`${payload.name} ,
+        ${payload.randomNum},
+        ${payload.txt}
+        `
+        );
       });
     });
+
+    socket.on('replaiedtoUsers', (payload) => {
+      this.setState({
+        msgs: [...this.state.msgs, payload],
+        showMsgs: true
+      });
+
+
+    }
+
+
+    )
+
   }
   handleChange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
@@ -28,96 +58,226 @@ class Home extends React.Component {
     e.preventDefault();
     const payload = {
       ...this.state,
-      created_at: Date.now(),
+      created_at: Date.now().toLocaleString(),
     };
-    console.log('hello', payload);
-    // once the user submit the form we need to emit a ticket so all TAs can see that ticket
+    console.log('hello', this.state.Destination);
 
-    // 1
-    socket.emit('createTicket', payload);
+    socket.emit('sendRequest', payload);
   };
+
+  handleMsg = async (e) => {
+    await this.setState({ msg: e.target.value });
+
+  }
+
+
+  updatestate = (e) => {
+
+    this.setState({ AllMsg: [...this.state.AllMsg, this.state.msg] })
+    // this.setState({msg:''});
+
+  }
+  sendMsg = async (e) => {
+
+    e.preventDefault();
+    await this.updatestate()
+
+
+    const payload = {
+      msg: this.state.msg,
+      created_at: Date.now(),
+      userName: this.state.clientName
+    };
+    console.log(payload.msg)
+    socket.emit('sendmsg', payload);
+
+  }
+
   render() {
     return (
-      <main className="container">
-        <section className="form-card">
-          <form id="questions-form" onSubmit={this.handleSubmit}>
-            {/* <input
-              className="Countries"
-              type="text"
-              name="question"
-              placeholder="write your question here..."
-              required
-              onChange={this.handleChange}
-            /> */}
-            <label className="lab">
-              <input
-                type="checkbox"
-                name="cityname"
-                value="Amman"
-                
-                onChange={this.handleChange}
-              />
-              Amman
-            </label>
-            <label className="cc">
-              <input
-                type="checkbox"
-                name="cityname"
-                value="Aqaba"
-                onChange={this.handleChange}
-              />
-              Aqaba
-            </label>
-            <label className="course">
-              <input
-                type="checkbox"
-                name="cityname"
-                value=" Wadi Rum"
-                
-                onChange={this.handleChange}
-              />
-              Wadi Rum
-            </label>
-            <label className="course">
-              <input
-                type="checkbox"
-                name="flight"
-                value="Qatar Airlines"
-                
-                onChange={this.handleChange}
-              />
-              Qatar Airlines
-            </label>
-            <label className="course">
-              <input
-                type="checkbox"
-                name="flight"
-                value="Fly Emirates"
-                onChange={this.handleChange}
-              />
-              Fly Emirates
-            </label>
-            <label className="course">
-              <input
-                type="checkbox"
-                name="flight"
-                value=" Jordanian Royal"
-                onChange={this.handleChange}
-              />
-              Jordanian Royal
-            </label>
+      <div >
+        <main className="container">
+          <section className="form-card boxShadow"  >
 
-            {/* <label for="vehicle1">Fly Emirates </label><br/>
-            <input type="checkbox" name="location"  value="Fly Emirates" onChange={this.handleChange}/>
 
-            <label for="vehicle2">Qatar Airlines</label><br/>
-                <input type="checkbox" name="location"  value="Qatar Airlines" onChange={this.handleChange}/> */}
+            <Form onSubmit={this.handleSubmit}>
+              <Form.Row>
+                <Form.Group as={Col} controlId="formGridEmail" >
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control
+                    className="yourName"
+                    type="text"
+                    name="clientName"
+                    placeholder="write your name here..."
+                    onChange={this.handleChange}
+                  />
+                </Form.Group>
 
-            <button className="question">help!</button>
 
-          </form>
-        </section>
-      </main>
+
+                <Form.Group controlId="formGridAddress1">
+                  <Form.Label>Address</Form.Label>
+                  <Form.Control
+                    className="yourName"
+                    type="text"
+                    name="address"
+                    placeholder="write your address here..."
+                    onChange={this.handleChange}
+                  />
+                </Form.Group>
+
+                {/* <Form.Group controlId="formGridAddress2">
+    <Form.Label>Address 2</Form.Label>
+    <Form.Control placeholder="Apartment, studio, or floor" />
+  </Form.Group> */}
+              </Form.Row>
+
+              <Form.Row>
+                <Form.Group as={Col} controlId="formGridCity">
+                  <Form.Label>Phone Number</Form.Label>
+                  <Form.Control
+                    className="center"
+                    type="text"
+                    name="phone"
+                    placeholder="write your address here..."
+                    onChange={this.handleChange}
+                  />
+                </Form.Group>
+
+              </Form.Row>
+              <fieldset >
+                <Form.Group >
+                  <Form.Label as="legend">
+                    Airlines:
+                  </Form.Label>
+
+                  <Form.Check
+                    className="center"
+                    type="radio"
+                    label="Fly Emirates"
+                    name="Airlines"
+                    value="Fly Emirates"
+                    id="formHorizontalRadios1"
+                    onChange={this.handleChange}
+
+                  />
+
+                  <Form.Check
+                    className="center"
+                    type="radio"
+                    label="Qatar Airlines"
+                    name="Airlines"
+                    value="Qatar Airlines"
+                    id="formHorizontalRadios1"
+                    onChange={this.handleChange}
+
+                  />
+                  <Form.Check
+
+                    className="center"
+                    type="radio"
+                    label="Jordanian Royal"
+                    name="Airlines"
+                    value="Jordanian Royal"
+                    id="formHorizontalRadios1"
+                    onChange={this.handleChange}
+
+
+                  />
+
+                </Form.Group>
+              </fieldset>
+
+
+              <fieldset className="center">
+                <Form.Group>
+                  <Form.Label as="legend">
+                    Destination:       </Form.Label>
+
+                  <Form.Check
+                    type="radio"
+                    label=" UK"
+                    value="UK"
+                    name="Destination"
+                    id="formHorizontalRadios1"
+                    onChange={this.handleChange}
+                  />
+
+                  <Form.Check
+                    type="radio"
+                    label="USA"
+                    value="USA"
+                    name="Destination"
+                    id="formHorizontalRadios1"
+                    onChange={this.handleChange}
+                  />
+                  <Form.Check
+                    type="radio"
+                    label="DE"
+                    value="DE"
+                    name="Destination"
+                    id="formHorizontalRadios1"
+                    onChange={this.handleChange}
+                  />
+
+                </Form.Group>
+              </fieldset>
+
+
+
+
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+
+
+
+
+
+
+          </section>
+
+
+        </main>
+
+
+        <div className="feedback">
+          <h3>Leave A feedback</h3>
+          <input
+            type="text"
+            onChange={this.handleMsg}
+          />
+          <button onClick={this.sendMsg}>Send </button>
+
+
+          <br></br>
+          <h2>test</h2>
+
+          {
+            this.state.showMsgs &&
+
+            this.state.msgs.map((msg) => {
+              return (
+                <div className="feedbackmsg">
+                  <p>{msg.userName2}: {msg.msg2} </p>
+                  <p> {"Admin : "} {msg.msg} </p>
+                </div>
+              )
+            })
+          }
+
+
+          <Navbar bg="dark" variant="dark">
+            <Navbar.Brand className="centerize">All Rights Reserved</Navbar.Brand>
+          </Navbar>
+
+
+
+        </div>
+
+      </div>
+
     );
   }
 }

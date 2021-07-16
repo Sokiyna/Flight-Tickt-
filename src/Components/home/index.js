3d05891f036370e8b785c4adf9b430a77a3f8a65
Sokiyna <sokiynanaser@yahoo.com>
2021-07-16 19:43:34 +0300

    deployfront

--- src/Components/home/index.js
+++ src/Components/home/index.js
@@ -10,7 +10,6 @@ import {Form,Button,Col} from 'react-bootstrap/'
 import io from 'socket.io-client';
 const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL || 'http://localhost:5000/';
 const socket = io(REACT_APP_SERVER_URL, { transports: ['websocket'] });
-log
 
 class Home extends React.Component {
   constructor(props) {

0ac8a7eb4ff96ce8917cc67f49f78c62cbf07275
Sokiyna <sokiynanaser@yahoo.com>
2021-07-16 19:41:39 +0300

    deployfront

--- src/Components/home/index.js
+++ src/Components/home/index.js
@@ -8,8 +8,9 @@ import {NotificationContainer, NotificationManager} from 'react-notifications';
 
 import {Form,Button,Col} from 'react-bootstrap/'
 import io from 'socket.io-client';
-const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL || 'localhost:5000/';
+const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL || 'http://localhost:5000/';
 const socket = io(REACT_APP_SERVER_URL, { transports: ['websocket'] });
+log
 
 class Home extends React.Component {
   constructor(props) {
